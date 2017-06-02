f62ec2feaa76df855f569b07100a62eb51dd009a
“Erika <foglzerika@gmail.com>
2017-06-02 12:25:20 -0500

    Agregando el texto

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 
+
+
+var enviaFormulario = function(){
+	console.log("Yeiii ya envie el formulario");
+	$nuevoContacto = $("#listaContactos");
+	//Agrego el nuevo elemento
+	$nuevoContacto.append("<div>");
+	var $nuevaSeccion = $nuevoContacto[0].className="contacto";
+	$nombre = $("#nombre").val();
+	$apellido = $("#apellido").val();
+	$telefono = $("#telefono").val();
+	$email = $("#email").val();
+	$nuevoContacto.append("<p>"+$nombre+"</p>");
+	$nuevoContacto.append("<p>"+$apellido+"</p>");
+	$nuevoContacto.append("<p>"+$telefono+"</p>");
+	$nuevoContacto.append("<p>"+$email+"</p>");
+}
+ 
 var cargaPagina = function(){
 	$('.modal').modal();
+	$("#agregar").click(enviaFormulario);
 }
 
 
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    cargaPagina();
- });
+$(document).ready(cargaPagina);
         
